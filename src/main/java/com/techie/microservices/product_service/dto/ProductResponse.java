4d65086de873981ba6fa9c4a4e5d2ff3766f29fa
Rami Badri <rami@Mac.Home>
2025-12-18 11:23:17 -0800

    fixed body request and response

--- src/main/java/com/techie/microservices/product_service/dto/ProductResponse.java
+++ src/main/java/com/techie/microservices/product_service/dto/ProductResponse.java
@@ -1,4 +1,6 @@
 package com.techie.microservices.product_service.dto;
 
-public record ProductResponse() {
+import java.math.BigDecimal;
+
+public record ProductResponse(String id, String name, String description, BigDecimal price) {
 }
